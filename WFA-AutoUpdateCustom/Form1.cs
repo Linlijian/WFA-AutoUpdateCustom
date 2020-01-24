758dc21e622453a61b82d1663768d7d3a18a067e
linlijian <praw__Rul2__lvluk@Outlook.co.th>
2020-01-24 11:20:28 +0700

    Add project files.

---
+++ WFA-AutoUpdateCustom/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AutoUpdaterDotNET;
+namespace WFA_AutoUpdateCustom
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            AutoUpdater.Start("https://raw.githubusercontent.com/Linlijian/WFA-AutoUpdateCustom/master/WFA-AutoUpdateCustom/CheckUpdate.xml");            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            AutoUpdater.Start();
+            label1.Text = Application.ProductVersion;
+        }
+    }
+}
