716fd09f1aa9d63a142dd1cdcc686e41eb17ebdb
linlijian <praw__Rul2__lvluk@Outlook.co.th>
2020-01-24 14:37:29 +0700

    end

--- WFA-AutoUpdateCustom/Form1.cs
+++ WFA-AutoUpdateCustom/Form1.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using AutoUpdaterDotNET;
+using System.Diagnostics;
+
 namespace WFA_AutoUpdateCustom
 {
     public partial class Form1 : Form
@@ -15,13 +17,77 @@ namespace WFA_AutoUpdateCustom
         public Form1()
         {
             InitializeComponent();
-            AutoUpdater.Start("https://raw.githubusercontent.com/Linlijian/WFA-AutoUpdateCustom/master/WFA-AutoUpdateCustom/CheckUpdate.xml");            
+            AutoUpdater.UpdateFormSize = new System.Drawing.Size(740, 466);
+            AutoUpdater.Start("https://raw.githubusercontent.com/Linlijian/WFA-AutoUpdateCustom/master/WFA-AutoUpdateCustom/CheckUpdate.xml");
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            AutoUpdater.Start();
+            // AutoUpdater.Start();
+            AutoUpdater.CheckForUpdateEvent += AutoUpdaterOnCheckForUpdateEvent;
             label1.Text = Application.ProductVersion;
+        }
+        
+        private void AutoUpdaterOnCheckForUpdateEvent(UpdateInfoEventArgs args)
+        {
+            if (args != null)
+            {
+                if (args.IsUpdateAvailable)
+                {
+                    DialogResult dialogResult;
+                    if (args.Mandatory)
+                    {
+                        dialogResult =
+                            MessageBox.Show(
+                                $@"There is new version {args.CurrentVersion} available. You are using version {args.InstalledVersion}. This is required update. Press Ok to begin updating the application.", @"Update Available",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        dialogResult =
+                            MessageBox.Show(
+                                $@"There is new version {args.CurrentVersion} available. You are using version {
+                                        args.InstalledVersion
+                                    }. Do you want to update the application now?", @"Update Available",
+                                MessageBoxButtons.YesNo,
+                                MessageBoxIcon.Information);
+                    }
+
+                    // Uncomment the following line if you want to show standard update dialog instead.
+                    AutoUpdater.ShowUpdateForm();
+
+                    ProcessStartInfo sInfo = new ProcessStartInfo("https://stackoverflow.com/questions/58024/open-a-url-from-windows-forms");
+                    Process.Start(sInfo);
+
+                    if (dialogResult.Equals(DialogResult.Yes) || dialogResult.Equals(DialogResult.OK))
+                    {
+                        try
+                        {
+                            if (AutoUpdater.DownloadUpdate())
+                            {
+                                Application.Exit();
+                            }
+                        }
+                        catch (Exception exception)
+                        {
+                            MessageBox.Show(exception.Message, exception.GetType().ToString(), MessageBoxButtons.OK,
+                                MessageBoxIcon.Error);
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show(@"There is no update available please try again later.", @"No update available",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            else
+            {
+                MessageBox.Show(
+                        @"There is a problem reaching update server please check your internet connection and try again later.",
+                        @"Update check failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
